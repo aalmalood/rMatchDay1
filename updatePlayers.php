507b49b3dd3704df96bd1a5370eb58a76922e5b8
aalmalood <almalood.91@gmail.com>
2023-12-07 12:15:47 +0300

    json

--- updatePlayers.php
+++ updatePlayers.php
@@ -1,14 +1,14 @@
 <?php
 header("Access-Control-Allow-Origin: *");
 header("Content-Type: application/json; charset=UTF-8");
-
-$json = $_GET['players'];
+$postdata = file_get_contents("php://input");
+$request = json_decode($postdata);
 
 /* sanity check */
 if (json_decode($json) != null)
 {
   $file = fopen('p1.json','w+');
-  fwrite($file, $json);
+  fwrite($file, $request->players);
   fclose($file);
 }
 else
